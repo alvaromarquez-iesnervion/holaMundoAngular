f6592a619a9fd1e1cbed0a5afebc7aff78c6863f
alvaromarquez-iesnervion <alvaro.marquez@iesnervion.es>
2026-01-12 14:40:56 +0100

    jabvfai

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TablaPersonas } from './components/tabla-personas/tabla-personas';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TablaPersonas],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
